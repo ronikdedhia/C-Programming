9b0547357301c264ca249eff05db2c5e431308c7
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-04-08 23:38:15 +0530

    setting up environment and testing c

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+C is a powerful general-purpose programming language.
+It can be used to develop software like operating systems, databases, compilers, and so on.
+It is an excellent language to learn to program for beginners.
+*/
+int main() {
+   printf("Hello, World!");
+   return 0;
+}

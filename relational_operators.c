e8908290419c251d59287e879f78aeceb39c8357
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-04-08 23:38:59 +0530

    working on basic relational operations

---
+++ relational_operators.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+relational operator checks the relationship between two operands.
+if the relation is true, it returns 1; if the relation is false, it returns value 0.
+*/
+int main()
+{
+    int a = 5, b = 5, c = 10;
+    printf("%d == %d is %d \n", a, b, a == b);
+    printf("%d == %d is %d \n", a, c, a == c);
+    printf("%d > %d is %d \n", a, b, a > b);
+    printf("%d > %d is %d \n", a, c, a > c);
+    printf("%d < %d is %d \n", a, b, a < b);
+    printf("%d < %d is %d \n", a, c, a < c);
+    printf("%d != %d is %d \n", a, b, a != b);
+    printf("%d != %d is %d \n", a, c, a != c);
+    printf("%d >= %d is %d \n", a, b, a >= b);
+    printf("%d >= %d is %d \n", a, c, a >= c);
+    printf("%d <= %d is %d \n", a, b, a <= b);
+    printf("%d <= %d is %d \n", a, c, a <= c);
+    return 0;
+}

b729252519d46e4be1514c12502b883fb444e414
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-04-12 00:50:55 +0530

    accept principle, rate of interest, time and computes the simple interest

---
+++ simple_interest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/*
+program that accepts principle, rate of interest, time and computes the simple interest.
+*/
+int main()
+{
+int p,r,t,int_amt;
+printf("Input principle, Rate of interest & time to find simple interest: \n");
+scanf("%d%d%d",&p,&r,&t);
+int_amt=(p*r*t)/100;
+printf("Simple interest = %d",int_amt);
+return 0;
+}

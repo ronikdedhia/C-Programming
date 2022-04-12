d1ce33233711fe0d48a8e44dee0f4f462c02dc2c
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-04-13 00:59:00 +0530

    methods for performing mathematical operations

---
+++ math_library.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <math.h>
+/*
+<math.h> header file contains various methods for performing mathematical operations
+*/
+int main(){
+printf("\n%f",ceil(3.6));
+printf("\n%f",ceil(3.3));
+printf("\n%f",floor(3.6));
+printf("\n%f",floor(3.2));
+printf("\n%f",sqrt(16));
+printf("\n%f",sqrt(7));
+printf("\n%f",pow(2,4));
+printf("\n%f",pow(3,3));
+printf("\n%d",abs(-12));
+ return 0;
+}

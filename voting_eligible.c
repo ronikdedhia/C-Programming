71004382dc22ae77f4cd50276bac1fb343efcaeb
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-04-14 12:08:42 +0530

    determine whether candidate is eligible for casting vote

---
+++ voting_eligible.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ read the age of a candidate and determine whether he is eligible for casting vote
+ */
+void main()
+{
+  int vote_age;
+
+  printf("Input the age of the candidate : ");
+  scanf("%d",&vote_age);
+  if (vote_age<18)
+     {
+       printf("Sorry, You are not eligible to caste your vote.\n");
+       printf("You would be able to caste your vote after %d year.\n",18-vote_age);
+     }
+  else
+     printf("Congratulation! You are eligible for casting your vote.\n");
+}

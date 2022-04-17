1496dc70a7be02a0240a066b1c208a179e0fa20f
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-04-17 22:35:00 +0530

    places smallest element at the beginning of the unsorted list

---
+++ selection_sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/*
+selection sort selects the smallest element from an unsorted list in each iteration and places that element at the beginning of the unsorted list
+*/
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+void selectionSort(int array[], int size) {
+  for (int step = 0; step < size - 1; step++) {
+    int min_idx = step;
+    for (int i = step + 1; i < size; i++) {
+      if (array[i] < array[min_idx])
+        min_idx = i;
+    }
+    swap(&array[min_idx], &array[step]);
+  }
+}
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
+int main() {
+  int data[] = {20, 12, 10, 15, 2};
+  int size = sizeof(data) / sizeof(data[0]);
+  selectionSort(data, size);
+  printf("Sorted array in Acsending Order:\n");
+  printArray(data, size);
+}

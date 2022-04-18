d1ca3527cd6da4ee36e5abfed6a94b6741532973
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-04-19 00:15:37 +0530

    calculate the power of a number using recursion

---
+++ power_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+calculate the power of a number using recursion.
+*/
+int power(int base, int a) {
+    if (a != 0)
+        return (base * power(base, a - 1));
+    else
+        return 1;
+}
+int main() {
+    int base, a, result;
+    printf("Enter base number: ");
+    scanf("%d", &base);
+    printf("Enter power number(positive integer): ");
+    scanf("%d", &a);
+    result = power(base, a);
+    printf("%d^%d = %d", base, a, result);
+    return 0;
+}

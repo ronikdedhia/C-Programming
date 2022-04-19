b287e65f678de2a84cd1e2eebe09a30ce1b3606e
Ronik Dedhia <79242111+ronikdedhia@users.noreply.github.com>
2022-04-20 00:09:24 +0530

    sort strings in lexicographical order

---
+++ alphabet_sort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+/*
+sort strings entered by the user in the lexicographical order (dictionary order)
+*/
+int main() {
+   char str[5][50], temp[50];
+   printf("Enter 5 words: ");
+   for (int i = 0; i < 5; ++i) {
+      fgets(str[i], sizeof(str[i]), stdin);
+   }
+   for (int i = 0; i < 5; ++i) {
+      for (int j = i + 1; j < 5; ++j) {
+         if (strcmp(str[i], str[j]) > 0) {
+            strcpy(temp, str[i]);
+            strcpy(str[i], str[j]);
+            strcpy(str[j], temp);
+         }
+      }
+   }
+   printf("\nIn the lexicographical order: \n");
+   for (int i = 0; i < 5; ++i) {
+      fputs(str[i], stdout);
+   }
+   return 0;
+}
